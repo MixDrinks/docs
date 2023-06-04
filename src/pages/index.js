78288f761c9416f54e54c642f8ece6567bff59a7
Vova Stelmashchuk <smallstells@gmail.com>
2023-06-04 20:38:04 +0300

    Fixed author and some meta info

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
     const {siteConfig} = useDocusaurusContext();
     return (
         <Layout
-            title={`Hello from ${siteConfig.title}`}
-            description="Description will go into a meta tag in <head />">
+            title={`Документація ${siteConfig.title}`}
+            description="Документація до backend, api, сайту та мобільних додатків MixDrinks.">
             <HomepageHeader/>
             <main>
                 <HomepageFeatures/>
